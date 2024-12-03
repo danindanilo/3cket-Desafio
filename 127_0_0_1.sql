7efc2991cc900fbe0eabd6aa2b1f285098651c55
danindanilo <77695614+danindanilo@users.noreply.github.com>
2024-12-03 20:03:45 +0000

    Add files via upload

---
+++ 127_0_0_1.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03-Dez-2024 às 21:03
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `3cket`
+--
+CREATE DATABASE IF NOT EXISTS `3cket` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `3cket`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `category`
+--
+
+INSERT INTO `category` (`id_category`, `name`) VALUES
+(2, 'Carros'),
+(3, 'Urbex'),
+(4, 'Animais');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `image`
+--
+
+CREATE TABLE `image` (
+  `id_image` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `cod_category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `image`
+--
+
+INSERT INTO `image` (`id_image`, `name`, `image`, `cod_category`) VALUES
+(51, 'Sanatório', 'uploads/IMG_3976.jpg', 3),
+(52, 'Medo', 'uploads/IMG_3977.jpg', 3),
+(53, 'M5', 'uploads/IMG_4516.jpg', 2),
+(57, 'MB', 'uploads/IMG_4509.jpg', 2),
+(58, 'MB', 'uploads/IMG_1282.jpg', 2),
+(60, 'Triumph', 'uploads/IMG_3772.jpg', 2),
+(61, 'lemur', 'uploads/IMG_0943.jpg', 4),
+(62, 'Cão', 'uploads/IMG_0390.jpg', 4),
+(63, 'Arara', 'uploads/IMG_0489.jpg', 4);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Índices para tabela `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id_image`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `image`
+--
+ALTER TABLE `image`
+  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
