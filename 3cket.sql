0c446dcc0fba971c2bd4a6102d06c9f0000b614d
danindanilo <77695614+danindanilo@users.noreply.github.com>
2024-12-03 20:01:20 +0000

    Add files via upload

--- 3cket.sql
+++ 3cket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Dez-2024 às 20:05
+-- Tempo de geração: 03-Dez-2024 às 20:59
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -37,7 +37,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id_category`, `name`) VALUES
-(41, 'Teste');
+(2, 'Carros'),
+(3, 'Urbex'),
+(4, 'Animais');
 
 -- --------------------------------------------------------
 
@@ -51,6 +53,21 @@ CREATE TABLE `image` (
   `image` varchar(255) NOT NULL,
   `cod_category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `image`
+--
+
+INSERT INTO `image` (`id_image`, `name`, `image`, `cod_category`) VALUES
+(51, 'Sanatório', 'uploads/IMG_3976.jpg', 3),
+(52, 'Medo', 'uploads/IMG_3977.jpg', 3),
+(53, 'M5', 'uploads/IMG_4516.jpg', 2),
+(57, 'MB', 'uploads/IMG_4509.jpg', 2),
+(58, 'MB', 'uploads/IMG_1282.jpg', 2),
+(60, 'Triumph', 'uploads/IMG_3772.jpg', 2),
+(61, 'lemur', 'uploads/IMG_0943.jpg', 4),
+(62, 'Cão', 'uploads/IMG_0390.jpg', 4),
+(63, 'Arara', 'uploads/IMG_0489.jpg', 4);
 
 --
 -- Índices para tabelas despejadas
@@ -76,13 +93,13 @@ ALTER TABLE `image`
 -- AUTO_INCREMENT de tabela `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `image`
 --
 ALTER TABLE `image`
-  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
